23b8f7605268a5a2b06c82a065e7b816a6336524
jeffreyesun@gmail.com <jesun@princeton.edu>
2020-12-18 18:23:58 -0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChessboardModule } from 'ng2-chessboard';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChessboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
