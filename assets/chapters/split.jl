6199d8563b8d19c7d43c9a245d1ed5bea428be07
jeffreyesun@gmail.com <jesun@princeton.edu>
2021-02-14 14:45:31 -0500

    Auto-generated commit

---
+++ assets/chapters/split.jl
@@ -0,0 +1,24 @@
+root = "src/assets/chapters/"
+
+fulltext = read(open(root*"alltext.txt", "r"), String)
+
+m = eachmatch(r"Chapter (\d|One|Two|Three)+\D", fulltext)
+
+collect(m)[5].offset
+
+chaps = []
+
+moff = [m.offset for m in m]
+mlen = [length(m.match) for m in m]
+
+for (mi,m) in enumerate(moff[1:end-1])
+    chap = fulltext[m+mlen[mi] : moff[mi+1]-1]
+    chap = strip(chap)
+    push!(chaps, chap)
+end
+
+for (i,chap) in enumerate(chaps)
+    open(root*"chap$i.txt", "w") do io
+        write(io, chaps[i])
+    end
+end
